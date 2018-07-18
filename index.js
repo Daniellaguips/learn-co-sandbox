b1c9e81cbe1c0d7691d461e2d1c7098495985323
daniellaguips@gmail.com Guimaraes <daniellaguips@gmail.com>
2018-07-18 22:06:18 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,15 @@
+
+  function sayHelloTo(firstName) {
+  console.log(`Hello, ${firstName}!`)
+}
+ 
+sayHelloTo('Isabel') 
+sayHelloTo("Jane") // 
+sayHelloTo("R2-D2") // 
+sayHelloTo(1) //
+  
+  function say(greeting, firstName) {
+    console.log (`${greeting}, ${firstName}!`)
+  }
+  
+  say("Goodbye","Julio")
